258c867f33a50a0e12f44ef8ced0cdd30a5f5aef
christian <hichristiangomez@gmail.com>
2026-05-25 13:27:12 -0400

    add comments to build-manifest.js for clarity on usage and functionality

--- build-manifest.js
+++ build-manifest.js
@@ -1,3 +1,7 @@
+// Run with: node build-manifest.js
+// Regenerates images.json from whatever files are currently in the images/ folder.
+// Run this anytime you add, remove, or rename photos before committing to GitHub.
+
 const fs = require('fs');
 const files = fs.readdirSync('./images')
   .filter(f => /\.(jpg|jpeg|png|webp|gif)$/i.test(f))
